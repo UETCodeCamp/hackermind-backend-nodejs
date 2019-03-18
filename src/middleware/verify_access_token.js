30145475a6244c48e9f28a39d5181c4fc90acdc5
Tien Quan <zed.quan99@gmail.com>
2019-03-18 22:29:14 +0700

    debug

--- src/middleware/verify_access_token.js
+++ src/middleware/verify_access_token.js
@@ -4,6 +4,7 @@ const key = require('../../key');
 
 function verifyAccessToken(req, res, next) {
     const token = req.headers["access_token"];
+    console.log(req.headers);
     console.log(token);
     if (token) {
         jwt.verify(token, key.ACCESS_SECRET_KEY, function (err, decoded) {
