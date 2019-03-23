173c4a5807f7935dc9713a782e9c81874b5e38d4
Tien Quan <zed.quan99@gmail.com>
2019-03-23 20:36:09 +0700

    add team_id to profile

--- src/action/user.js
+++ src/action/user.js
@@ -1,11 +1,17 @@
 const db = require('../database');
 
 module.exports.findUser = async (contrain) => {
+    const team = await db.TeamUserModel.findOne({
+        where: {
+            user_id: contrain.id
+        }
+    });
+    console.log(team);
     const user = await db.UserModel.findOne({
         where: contrain,
         attributes: ['user_name','id', 'avatar', 'name', 'email', 'point', 'description', 'role_id']
-
     });
+    user.dataValues.team_id = team.team_id;
     return user;
 };
 
